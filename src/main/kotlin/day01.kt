2fbacec083daea419cd4fd7c2320b8b7bafd7fd7
Oleksii_Borys <oleksii.borys.con@hotstar.com>
2023-12-01 17:39:13 +0200

    init

--- src/main/kotlin/day01.kt
+++ src/main/kotlin/day01.kt
@@ -40,6 +40,8 @@ val arr = listOf("1abc2",
 //The newly-improved calibration document consists of lines of text; each line originally contained a specific calibration value that the Elves now need to recover. On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
 //
 //For example:
+
+//In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
 //
 //1abc2
 //pqr3stu8vwx
